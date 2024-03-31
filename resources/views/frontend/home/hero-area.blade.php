62cd6763a7f2021afa75d32d32ebe404a9733e2b
Alan Diniz <alan@splitpaybrasil.com.br>
2024-03-30 23:09:01 -0300

    feat/updates

--- resources/views/frontend/home/hero-area.blade.php
+++ resources/views/frontend/home/hero-area.blade.php
@@ -5,15 +5,15 @@
             <div class="container">
                 <div class="hero-content">
                     <div class="section-heading">
-                        <h2 class="section__title text-white fs-65 lh-80 pb-3">We Help You Learn <br> What You Love</h2>
-                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
-                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Venham Aprender <br> Em casa</h2>
+                        <p class="section__desc text-white pb-4">Faça o curso em casa
+                            <br>Com os melhores professores
                         </p>
                     </div><!-- end section-heading -->
                     <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1">
-                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Join with Us <i class="la la-arrow-right icon ml-1"></i></a>
-                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
-                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
+                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Faça o Cadastro <i class="la la-arrow-right icon ml-1"></i></a>
+                        <a href="{{route('login')}}" class="btn-text video-play-btn mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
+                            Confira<i class="la la-play icon-btn ml-2"></i>
                         </a>
                     </div><!-- end hero-btn-box -->
                 </div><!-- end hero-content -->
@@ -23,17 +23,12 @@
             <div class="container">
                 <div class="hero-content text-center">
                     <div class="section-heading">
-                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Join Aduca & Get <br> Your Free Courses!</h2>
-                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
-                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Temos várias <br> Aulas</h2>
+                        <p class="section__desc text-white pb-4">Com forúm de dúvidas
+                            <br>Não perca tempo.
                         </p>
                     </div><!-- end section-heading -->
-                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-center">
-                        <a href="admission.html" class="btn theme-btn mr-4 mb-4">Get Started <i class="la la-arrow-right icon ml-1"></i></a>
-                        <a href="#" class="btn-text video-play-btn mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
-                            Watch Preview<i class="la la-play icon-btn ml-2"></i>
-                        </a>
-                    </div><!-- end hero-btn-box -->
+
                 </div><!-- end hero-content -->
             </div><!-- container -->
         </div><!-- end hero-slider-item -->
@@ -41,17 +36,11 @@
             <div class="container">
                 <div class="hero-content text-right">
                     <div class="section-heading">
-                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Learn Anything, <br> Anytime, Anywhere</h2>
-                        <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting industry orem Ipsum has been the
-                            <br>industry's standard dummy text ever sinceprinting and typesetting industry.
+                        <h2 class="section__title text-white fs-65 lh-80 pb-3">Equipe de Professores <br> 24 horas</h2>
+                        <p class="section__desc text-white pb-4">Equipe totalmente qualificada
+                            <br>Cursos separado por módulos
                         </p>
                     </div>
-                    <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-end">
-                        <a href="#" class="btn-text video-play-btn mr-4 mb-4" data-fancybox data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
-                            <i class="la la-play icon-btn mr-2"></i>Watch Preview
-                        </a>
-                        <a href="admission.html" class="btn theme-btn mb-4"><i class="la la-arrow-left icon mr-1"></i>Get Enrolled </a>
-                    </div><!-- end hero-btn-box -->
                 </div><!-- end hero-content -->
             </div><!-- container -->
         </div><!-- end hero-slider-item -->
